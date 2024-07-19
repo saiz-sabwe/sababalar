9c3e85a8e3fa96e11fa11b5af586780126fd0cf0
joyce sabwe <joycesabwe@gmail.com>
2024-07-19 15:58:06 +0100

    some changes

---
+++ src/Controller/AgentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AgentController extends AbstractController
+{
+    #[Route('/agent', name: 'app_agent')]
+    public function index(): Response
+    {
+        return $this->render('agent/index.html.twig', [
+            'controller_name' => 'AgentController',
+        ]);
+    }
+}
---
+++ src/Entity/Ticket.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TicketRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TicketRepository::class)]
+class Ticket
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
